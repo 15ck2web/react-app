548c1fba17113d407914e7f2a4d03cf381419d65
Quoc Dep Trai <tranquoc020297@gmail.com>
2018-06-11 20:44:02 +0700

    update controller

--- Server/Controller/SanPham.js
+++ Server/Controller/SanPham.js
@@ -13,3 +13,26 @@ exports.DocTatCaSanPham=(req,res)=>{
 
 
 
+var tinhtrangmodels=require('../Models/TinhTrang');
+
+exports.DocTatCaTinhTrangHoaDon=(req,res)=>{
+    var IDtinhtrang=req.params.IDtinhtrang;
+    tinhtrangmodels.DocTatCaTinhTrangHoaDon(IDtinhtrang,(err,data)=>{
+        if(err){
+            res.status(400).send(data);
+            return;
+        }
+        res.send(data);
+    });
+};
+exports.CapNhatTinhTrangHoaDon=(req,res)=>{
+    var value=req.body;
+    tinhtrangmodels.CapNhatTinhTrangHoaDon(value,(err,data)=>{
+        if(err){
+            res.status(400).send(data);
+            return;
+
+        }
+        res.send(data);
+    });
+};
