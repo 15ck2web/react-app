2a9897dfaa500f1cd1af2b8c57aee2ccd46c1e18
tranminhson <minhson97@gmail.com>
2018-06-12 16:03:51 +0700

    update san pham

--- Server/Controller/SanPham.js
+++ Server/Controller/SanPham.js
@@ -10,14 +10,9 @@ exports.DocTatCaSanPham=(req,res)=>{
         res.send(data);
     });
 };
-
-
-
-var tinhtrangmodels=require('../Models/TinhTrang');
-
-exports.DocTatCaTinhTrangHoaDon=(req,res)=>{
-    var IDtinhtrang=req.params.IDtinhtrang;
-    tinhtrangmodels.DocTatCaTinhTrangHoaDon(IDtinhtrang,(err,data)=>{
+exports.ThemSanPham=(req,res)=>{
+    var value=req.body;
+    sanphammodels.ThemSanPham(value,(err,data)=>{
         if(err){
             res.status(400).send(data);
             return;
@@ -25,13 +20,138 @@ exports.DocTatCaTinhTrangHoaDon=(req,res)=>{
         res.send(data);
     });
 };
-exports.CapNhatTinhTrangHoaDon=(req,res)=>{
+exports.SuaSanPham=(req,res)=>{
     var value=req.body;
-    tinhtrangmodels.CapNhatTinhTrangHoaDon(value,(err,data)=>{
+    sanphammodels.SuaSanPham(value,(err,data)=>{
         if(err){
             res.status(400).send(data);
             return;
+        }
+        res.send(data);
+    });
+};
+exports.XoaSanPham=(req,res)=>{
+    var value=req.body;
+    sanphammodels.XoaSanPham(value,(err,data)=>{
+        if(err){
+            res.status(400).send(data);
+            return;
+        }
+        res.send(data);
+    });
+};
+exports.TimKiemMotSanPham=(req,res)=>{
+    var IDProduct=req.params.IDProduct;
+    sanphammodels.TimKiemMotSanPham(IDProduct,(err,data)=>{
+        if(err){
+            res.status(400).send(data);
+            return;
+        }
+        res.send(data);
+    });
+};
+exports.TimKiemMaSanPhamChinh=(req,res)=>{
+    var IDKeyProduct=req.params.IDKeyProduct;
+    sanphammodels.TimKiemMaSanPhamChinh(IDKeyProduct,(err,data)=>{
+        if(err){
+            res.status(400).send(data);
+            return;
+        }
+        res.send(data);
+    });
+};
+exports.TimKiemSanPhamTheoHSX=(err,data)=>{
+    var IDHSX=req.params.IDHSX;
+    sanphammodels.TimKiemSanPhamTheoHSX(IDHSX,(err,data)=>{
+        if(err){
+            res.status(400).send(data);
+            return;
+        }
+        res.send(data);
+    });
+};
+exports.TimKiemMotSanPhamHSX=(req,res)=>{
+    var IDName=req.params.IDName;
+    sanphammodels.TimKiemMotSanPhamHSX(IDName,(err,data)=>{
+        if(err){
+            res.status(400).send(data);
+            return;
+        }
+        res.send(data);
+    });
+};
+exports.TimKiemSanPhamTheoGia=(req,res)=>{
+    var GiaBD=req.params.GiaBD;
+    var GiaKT=req.params.GiaKT;
+    sanphammodels.TimKiemTrangSanPhamGia(GiaBD,GiaKT,(err,data)=>{
+        if(err){
+            res.status(400).send(data);
+            return;
+        }
+        res.send(data);
+    });
+};
+exports.TimKiemSanPhamTHeoLoai=(req,res)=>{
+    var IDLoaiSanPham=req.params.IDLoaiSanPham;
+    sanphammodels.TimKiemSanPhamTHeoLoai(IDLoaiSanPham,(err,data)=>{
+        if(err){
+            res.status(400).send(data);
+            return;
+        }
+        res.send(data);
+    });
 
+};
+
+exports.TimKiemTrangSanPhamHSX=(req,res)=>{
+    var IDPageHSX=req.params.IDPageHSX;
+    var IDNameHSX=req.params.IDNameHSX;
+    sanphammodels.TimKiemTrangSanPhamHSX(IDNameHSX,IDPageHSX,(err,data)=>{
+        if(err){
+            res.status(400).send(data);
+            return;
+        }
+        res.send(data);
+    });
+};
+exports.TimKiemTrangSanPhamGia=(req,res)=>{
+    var IDPageGia=req.params.IDPageGia;
+    var GiaBD=req.params.GiaBD;
+    var GiaKT=req.params.GiaKT;
+    sanphammodels.TimKiemTrangSanPhamGia(IDPageGia,GiaBD,GiaKT,(err,data)=>{
+        if(err){
+            res.status(400).send(data);
+            return;
+        }
+        res.send(data);
+    });
+};
+exports.TimKiemTrangSanPhamLoai=(req,res)=>{
+    var IDPageLoai=req.params.IDPageLoai;
+    var IDLoai=req.req.params.IDLoai;
+    sanphammodels.TimKiemTrangSanPhamLoai(IDLoai,IDPageLoai,(err,data)=>{
+        if(err){
+            res.status(400).send(data);
+            return;
+        }
+        res.send(data);
+    });
+};
+exports.TimTrangSanPham=(req,res)=>{
+    var PageSanPHam=req.params.PageSanPHam;
+    sanphammodels.TimTrangSanPham(PageSanPHam,(err,data)=>{
+        if(err){
+            res.status(400).send(data);
+            return;
+        }
+        res.send(data);
+    });
+};
+exports.TimSoTrangSanpham=(req,res)=>{
+    sanphammodels.TimSoTrangSanPham((err,data)=>{
+        if(err){
+            res.status(400).send(data);
+            return;
         }
         res.send(data);
     });
